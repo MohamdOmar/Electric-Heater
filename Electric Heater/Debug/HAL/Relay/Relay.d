dfc755c2efc0338076efddafcbc2888700593c50
Omar <mohameomar2028.gmail.com>
2021-12-08 23:09:32 +0200

    Initial commit

---
+++ Electric Heater/Debug/HAL/Relay/Relay.d
@@ -0,0 +1,13 @@
+HAL/Relay/Relay.d HAL/Relay/Relay.o: ../HAL/Relay/Relay.c \
+ ../HAL/Relay/Relay.h ../Util/ATmega32_Cfg.h ../Util/Std_Types.h \
+ ../MCAL/Dio/Dio.h ../Util/Std_Types.h
+
+../HAL/Relay/Relay.h:
+
+../Util/ATmega32_Cfg.h:
+
+../Util/Std_Types.h:
+
+../MCAL/Dio/Dio.h:
+
+../Util/Std_Types.h:
---
+++ Electric Heater/Debug/HAL/Switches/Sw.d
@@ -0,0 +1,31 @@
+HAL/Switches/Sw.d HAL/Switches/Sw.o: ../HAL/Switches/Sw.c \
+ ../MCAL/Dio/Dio.h ../Util/Std_Types.h ../HAL/Switches/Sw.h \
+ ../Util/ATmega32_Cfg.h ../Util/Std_Types.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+../MCAL/Dio/Dio.h:
+
+../Util/Std_Types.h:
+
+../HAL/Switches/Sw.h:
+
+../Util/ATmega32_Cfg.h:
+
+../Util/Std_Types.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
