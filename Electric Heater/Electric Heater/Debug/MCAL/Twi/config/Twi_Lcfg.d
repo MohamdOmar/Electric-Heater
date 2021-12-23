c61e382fdbb20ce7a8c577ae6bda7fda348d97f3
Omar <mohameomar2028.gmail.com>
2021-12-24 00:38:46 +0200

    commit

--- Electric Heater/Electric Heater/Debug/MCAL/Twi/config/Twi_Lcfg.d
+++
@@ -1,9 +0,0 @@
-MCAL/Twi/config/Twi_Lcfg.d MCAL/Twi/config/Twi_Lcfg.o: \
- ../MCAL/Twi/config/Twi_Lcfg.c ../MCAL/Twi/static/Twi.h \
- ../Util/Std_Types.h ../MCAL/Twi/config/Twi_Pcfg.h
-
-../MCAL/Twi/static/Twi.h:
-
-../Util/Std_Types.h:
-
-../MCAL/Twi/config/Twi_Pcfg.h:
